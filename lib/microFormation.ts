3f53322b921a67ac62a1f904eadd397c8b590a6c
Victor <v.ceazurita@gmail.com>
2026-07-01 07:16:41 +0200

    Add Castilla 26-27 structure

--- lib/microFormation.ts
+++ lib/microFormation.ts
@@ -1,63 +1,67 @@
 export const microFormation = [
+  // ==========================
   // PORTERO
+  // ==========================
   {
     id: "POR",
     nombre: "Portero",
     posicion: "Portero",
-    left: "50%",
-    top: "90%",
+    left: "8%",
+    top: "50%",
   },
 
+  // ==========================
   // DEFENSA
-
+  // ==========================
   {
-    id: "LD",
-    nombre: "Lateral D.",
-    posicion: "LATERAL D.",
-    left: "18%",
-    top: "70%",
+    id: "LI",
+    nombre: "Lateral I.",
+    posicion: "LATERAL I.",
+    left: "26%",
+    top: "10%",
   },
 
   {
     id: "DFC1",
     nombre: "Central",
     posicion: "CENTRAL",
-    left: "39%",
-    top: "73%",
+    left: "23%",
+    top: "36%",
   },
 
   {
     id: "DFC2",
     nombre: "Central",
     posicion: "CENTRAL",
-    left: "61%",
-    top: "73%",
+    left: "23%",
+    top: "64%",
   },
 
   {
-    id: "LI",
-    nombre: "Lateral I.",
-    posicion: "LATERAL I.",
-    left: "82%",
-    top: "70%",
+    id: "LD",
+    nombre: "Lateral D.",
+    posicion: "LATERAL D.",
+    left: "26%",
+    top: "90%",
   },
 
+  // ==========================
   // CENTROCAMPO
-
+  // ==========================
   {
     id: "MCD",
     nombre: "6",
     posicion: "6",
-    left: "28%",
-    top: "48%",
+    left: "49%",
+    top: "22%",
   },
 
   {
     id: "MCI",
     nombre: "8",
     posicion: "8",
-    left: "43%",
-    top: "42%",
+    left: "41%",
+    top: "50%",
   },
 
   {
@@ -65,32 +69,33 @@ export const microFormation = [
     nombre: "10",
     posicion: "10",
     left: "57%",
-    top: "42%",
+    top: "50%",
   },
 
   {
     id: "EI",
     nombre: "11",
     posicion: "11",
-    left: "72%",
-    top: "48%",
+    left: "49%",
+    top: "78%",
   },
 
+  // ==========================
   // DELANTERA
-
+  // ==========================
   {
     id: "DC1",
     nombre: "9",
     posicion: "9",
-    left: "40%",
-    top: "18%",
+    left: "84%",
+    top: "34%",
   },
 
   {
     id: "DC2",
     nombre: "9",
     posicion: "9",
-    left: "60%",
-    top: "18%",
+    left: "84%",
+    top: "66%",
   },
 ];
