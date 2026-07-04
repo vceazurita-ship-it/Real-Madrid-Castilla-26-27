66dbbe5e54d8f378c7f2fdf21211f95b62da6a8a
Victor <v.ceazurita@gmail.com>
2026-07-04 17:12:07 +0200

    Add Castilla 26-27 structure

--- context/SesionLineUpContext.tsx
+++ context/SesionLineUpContext.tsx
@@ -229,7 +229,7 @@ const [initialized, setInitialized] =
   function initializeFromPlayers(
     players: Player[]
   ) {
-
+    if (initialized) return;
     const newLineup =
       createLineup(formation);
 
@@ -248,9 +248,6 @@ if (!options) {
   );
   return;
 }
-
-      if (!options) return;
-
       const slot =
         findFreePosition(
           newLineup,
