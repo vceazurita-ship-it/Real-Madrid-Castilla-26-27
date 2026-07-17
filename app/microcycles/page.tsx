4919875ae231e8fbe8dd36d574e4059cde361586
Victor <v.ceazurita@gmail.com>
2026-07-17 22:59:01 +0200

    Cambio

--- app/microcycles/page.tsx
+++ app/microcycles/page.tsx
@@ -32,7 +32,7 @@ import {
   ZAxis,
 } from "recharts";
 const CSV_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3_1ScOV6sTyEpZSgLgCf2dKbwkLzb3zUEYM-7ZOoMbcFUTp7nvu1pBfGOP7EzppXXQYQhLeVa_SPr/pub?gid=0&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3_1ScOV6sTyEpZSgLgCf2dKbwkLzb3zUEYM-7ZOoMbcFUTp7nvu1pBfGOP7EzppXXQYQhLeVa_SPr/pub?gid=111318766&single=true&output=csv";
 
 const COLORS = {
   gold: "#C8A96B",
